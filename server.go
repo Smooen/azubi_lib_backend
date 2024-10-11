ed40ed7e36cc38bf4c1e3fee964cf25e3373372e
jogml <jonathan.gmehling@l-mobile.com>
2024-10-11 15:34:45 +0200

    Adding getBooks function

--- server.go
+++ server.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"net/http"
-	
+
 	"github.com/labstack/echo/v4"
 )
+
+var books =[]Book { 
+	{Isbn: "9780441172719", Title: "Dune", Author: "Frank Herbert", ReleaseDate: "1987", Availability: false}, 
+}
+
+func getBooks(c echo.Context) error {
+	return c.JSON(http.StatusOK, books);
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	
+	e.GET("/Books", getBooks)
+
 	e.Logger.Fatal(e.Start(":1323"))
+	
 }
 
 type Book struct {
@@ -19,5 +31,5 @@ type Book struct {
 	Isbn string `json:"isbn" query:"isbn"`
 	Author string `json:"author" query:"author"`
 	ReleaseDate string `json:"releaseDate" query:"releaseDate"`
-	Availability bool string `json:"availability" query:"availability"`
+	Availability bool `json:"availability" query:"availability"`
 }
