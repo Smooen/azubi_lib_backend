522abfdbeb5cebbf13bfb981dfb929ff16a8e43a
Smooen <smooenus@gmail.com>
2024-10-15 13:07:56 +0200

    add CORS from middleware

--- server.go
+++ server.go
@@ -114,6 +114,8 @@ func main() {
 	
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.Use(middleware.CORS())
 	
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
