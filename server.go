ce223a5d75af56b5e661b01b3aaf82da299a22fe
jogml <148053607+jogml@users.noreply.github.com>
2024-10-21 11:07:18 +0200

    Revert "Fb sql implementation"

--- server.go
+++ server.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"database/sql"
-	"log"
-	"os"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	
-	"github.com/joho/godotenv"
-
-	_ "github.com/microsoft/go-mssqldb"
 )
 
 var books =[]Book { 
@@ -24,41 +16,10 @@ func getBooks(c echo.Context) error {
 }
 
 func main() {
-	err := godotenv.Load("db.env")
-	if err != nil {
-		log.Fatal("Error loading .env files")
-	}
-
 	e := echo.New()
 	
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	e.Use(middleware.CORS())
-
-	server := os.Getenv("SERVER_NAME")
-	database := os.Getenv("DATABASE_NAME")
-	user := os.Getenv("USER_ID")
-
-	log.Printf("Beep boop, connecting to database")
-	log.Printf("Server:%s - database:%s", server, database)
-
-
-	connStr := fmt.Sprintf("server=%s;database=%s;user id=%s;", server, database, user)
-
-	db, err := sql.Open("mssql", connStr)
-	if err != nil {
-		log.Fatalf("Failed to open database connection: %v", err)
-	}
-
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
-	}
-    fmt.Println("Connected!")
-
-	defer db.Close()
-
 	
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
