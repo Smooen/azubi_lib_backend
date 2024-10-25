f30de488bf5ec6ff9dc828de68927d5f02f71c53
Smooen <smooenus@gmail.com>
2024-10-25 14:43:48 +0200

    migrate and make first query with gorm

--- db_create.go
+++ db_create.go
@@ -1,59 +1,51 @@
 package main
 
 import (
-	"database/sql"
+	//"database/sql"
 
-	"github.com/labstack/gommon/log"
+	//"github.com/labstack/gommon/log"
+
+	//"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-// Ideally this would be migrations, but for now this should be fine
-// https://gorm.io/docs/migration.html
-// TODO: Use gorm migrator instead of raw sql
+func migrate(db *gorm.DB) {
 
-func createBooksTable(db *sql.DB) {
-	tableExistsStmt := `SELECT count(name) FROM sqlite_master WHERE type='table' AND name='Books';`
+	db.AutoMigrate(&Book{})
 
-	row:= db.QueryRow(tableExistsStmt)
-	var exists int 
-	row.Scan(&exists)
-
-	if (exists == 1) { 
-		log.Print("Table alreay exists, skipping creation")
-		return 
-	}
-
-	createStmt := `
-		CREATE TABLE Books (
-			ISBN nvarchar(17) not null,
-			Title nvarchar(255) not null,
-			Author nvarchar(100) null,
-			ReleaseDate date null,
-			Availability bool not null
-		);
-	`
-
-	_, err := db.Exec(createStmt)
-	if err != nil {
-		log.Errorf("%q: %s\n", err, createStmt)
-		return
-	}
-
-	insertTestData(db)
+	createTestData(db)
 }
 
-func insertTestData(db *sql.DB) {
-	insertDataStmt := `
-		INSERT INTO Books
-		VALUES 
-			( '9783641279110', '1984', 'George Orwell', 'June 8, 1949', 1),
-			( '9780198826736', 'Pride and Prejudice', 'Jane Austen', 'January 28, 1813', 0),
-			( '9780007322596', 'The Lord of the Rings', 'J. R. R. Tolkien', 'July 29, 1954', 1),
-			( '9781438119250', 'The Catcher in the Rye', 'J. D. Salinger', 'July 16, 1951', 0)
-	`
+func createTestData(db *gorm.DB) {
 
-	_, err := db.Exec(insertDataStmt)
-	if err != nil {
-		log.Errorf("%q: %s\n", err, insertDataStmt)
-		return
-	}
+	db.Create(&Book{
+		Title:  "The Catcher in the Rye",
+		Isbn: "9781438119250",
+		Author: "J. D. Salinger",
+		ReleaseDate: "July 16, 1951",
+		Availability: true,
+	})
+
+	db.Create(&Book{
+		Title:  "The Lord of the Rings",
+		Isbn: "9780007322596",
+		Author: "J. R. R. Tolkien",
+		ReleaseDate: "July 29, 1954",
+		Availability: false,
+	})
+	
+	db.Create(&Book{
+		Title:  "Pride and Prejudice",
+		Isbn: "9780198826736",
+		Author: "Jane Austen",
+		ReleaseDate: "January 28, 1813",
+		Availability: false,
+	})
+	db.Create(&Book{
+		Title:  "1984",
+		Isbn: "9783641279110",
+		Author: "George Orwell",
+		ReleaseDate: "1949",
+		Availability: true,
+	})
 }
--- server.go
+++ server.go
@@ -3,31 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	
+
 	"github.com/joho/godotenv"
 
 	_ "github.com/mattn/go-sqlite3"
-	"gorm.io/gorm"
 	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-type Books struct {
+type (
+	Handler struct {
+		DB *gorm.DB
+	}
+)
+
+type Book struct {
 	gorm.Model
-	Title  string
-	Isbn string
-	Author string
-	ReleaseDate string
-	Availability bool
+	Title  string		`json:"title" query:"title"`
+	Isbn string			`json:"isbn" query:"isbn"`
+	Author string		`json:"author" query:"author"`
+	ReleaseDate string	`json:"releaseDate" query:"releaseDate"`
+	Availability bool	`json:"availability" query:"availability"`
 }
 
-var books =[]Book { 
-	{Isbn: "9780441172719", Title: "Dune", Author: "Frank Herbert", ReleaseDate: "1987", Availability: false}, 
-}
+func (h *Handler) getBooks(c echo.Context) error {
+	var books []Book
 
-func getBooks(c echo.Context) error {
+	db := h.DB
+	result := db.Find(&books)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, "Query failed")
+	}
+
 	return c.JSON(http.StatusOK, books);
 }
 
@@ -44,37 +55,26 @@ func main() {
 
 	e.Use(middleware.CORS())
 
-	//db, err := sql.Open("sqlite3", "./test.sqlite")
+	os.Remove("./test.sqlite")
 	db, err := gorm.Open(sqlite.Open("./test.sqlite"), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	db.Create(&Books{
-		Title:  "Test",
-		Isbn: "91203001",
-		Author: "Ich habs geschrieben",
-		//ReleaseDate: "2024",
-		Availability: true,
-	})
+	h := Handler{DB: db} //why even bother with handler if no session?
 
-	//createBooksTable(db)
+	migrate(db) // should migrate return err since it might fail?
+
+	var book Book
+	db.First(&book)
+	log.Printf("Found book: %s\n", book.Title)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	
-	e.GET("/Books", getBooks)
+	e.GET("/Books", h.getBooks)
 
 	e.Logger.Fatal(e.Start(":1323"))
 	
-}
-
-type Book struct {
-	Title  string `json:"title" query:"title"`
-	Isbn string `json:"isbn" query:"isbn"`
-	Author string `json:"author" query:"author"`
-	ReleaseDate string `json:"releaseDate" query:"releaseDate"`
-	Availability bool `json:"availability" query:"availability"`
 }
