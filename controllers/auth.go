7687b67f013aa2e2546731b3d44eb22987c2a1d5
Smooen <smooenus@gmail.com>
2025-01-23 09:21:53 +0100

    add first test of login

--- controllers/auth.go
+++ controllers/auth.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"azubi_library/models"
 	"net/http"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
@@ -18,9 +19,34 @@ type (
 
 type jwtCustomClaims struct {
 	Username  string `json:"username"`
-	Admin bool   `json:"admin"`
+	//Admin bool   `json:"admin"`
 	jwt.RegisteredClaims
 }
+
+func generateToken(user *models.User, secret []byte, c echo.Context) (string, error) {
+	claims := &jwtCustomClaims{
+		Username: user.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 72)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte("secret")) // secret should be key from env?
+	if err != nil {
+		c.JSON(500, "Could not generate token")
+		return "", err
+	}
+
+	return t, err
+}
+
+// func generateAccessToken(user *models.User) (string, time.Time, error) {
+// 	expirationTime := time.Now().Add(72 * time.Hour)
+//
+//
+// }
 
 func (h *Handler) Login(c echo.Context) error {
 	var user models.User
